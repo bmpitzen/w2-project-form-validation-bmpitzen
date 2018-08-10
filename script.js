f058cfc8cd77c9cc273b06b95de1088b028c957b
bmpitzen <bmpitzen@icloud.com>
2018-08-09 21:14:35 -0400

    Code works for invalid/valid. Working on conditions for car area

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 console.log('Add validation!');
 document.getElementById('parking-form').addEventListener('submit', function (event) {
-    event.preventDefault()
+event.preventDefault()
 
-    // clearError()
+var inputs = document.getElementsByTagName('input')
+
+for (input of inputs) {
+    console.log(input)
+
+    if (input.value === '') {
+        input.parentElement.classList.add('input-invalid')
+
+    } else {
+        input.parentElement.classList.add('input-valid')
+    }
+}
+
+})
+
+// clearError()
 
 // var name = document.getElementById('name').value.trim()
 
@@ -20,23 +35,26 @@ document.getElementById('parking-form').addEventListener('submit', function (eve
 // } else {
 //     testParent.classList.add ('input-valid')
 // }
-var fields = document.getElementsByClassName('input-field')
 
-for (i of fields){
-    debugger
-    var input = i.getElementsByTagName('input')[0].value.trim()
-    console.log (input)
-    
-    if (input === ''){
-        i.classList.add ('input-invalid')
-        
-    } else {
-        i.classList.add ('input-valid')
-    }
-}
 
-})
+// // this crap works!!! 👇🏻👇🏻👇🏻
+// var fields = document.getElementsByClassName('input-field')
 
+// for (i of fields){
+//     debugger
+//     var input = i.getElementsByTagName('input')[0].value.trim()
+//     console.log (input)
+
+//     if (input === ''){
+//         i.classList.add ('input-invalid')
+
+//     } else {
+//         i.classList.add ('input-valid')
+//     }
+// }
+
+// })
+// // 👆👆👆
 
 
 
@@ -46,5 +64,3 @@ for (i of fields){
 //     var errorDiv = document.getElementsByClassName('input-field')
 //     errorDiv.classList.add('invalid-input')
 // }
-
-
